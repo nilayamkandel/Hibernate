88e752f06f75a146a3032d62af3fea71ac53e005
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-16 21:38:50 +0545

    add:app config

---
+++ springdemo/src/main/java/annotation/AppConfig.java
@@ -0,0 +1,10 @@
+package annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //starts spring container
+@ComponentScan(basePackages = {"annotation"})
+public class AppConfig {
+
+}
