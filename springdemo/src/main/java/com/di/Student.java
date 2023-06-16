c178d43575126ed170233674d4ff0f7b42d73c28
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-16 21:40:13 +0545

    add:student file

---
+++ springdemo/src/main/java/com/di/Student.java
@@ -0,0 +1,50 @@
+package com.di;
+
+public class Student {
+	
+	private int id;
+	private String fname;
+	private String lname;
+	private String college;
+	private Address address;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", fname=" + fname + ", lname=" + lname + ", college=" + college + ", address="
+				+ address + "]";
+	}
+	
+}
