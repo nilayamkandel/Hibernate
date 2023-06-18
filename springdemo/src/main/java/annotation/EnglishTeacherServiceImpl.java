e9566ac2369d6f0cf1286efa544c96c28e8f53f2
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-18 08:42:26 +0545

    primary annotation

--- springdemo/src/main/java/annotation/EnglishTeacherServiceImpl.java
+++ springdemo/src/main/java/annotation/EnglishTeacherServiceImpl.java
@@ -1,8 +1,10 @@
 package annotation;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class EnglishTeacherServiceImpl implements TeacherService{
 
 	@Override
